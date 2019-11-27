43970eacb5aa7b0b252e0f21f7f6d06b34894f9c
Tommi Laurila <tommilaurila@gmail.com>
2019-11-27 22:27:39 +0200

    Updates

---
+++ precache-manifest.5bd45afc53949518f82adfad873f8827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1286b6968e0f6d06bd23f7ea65c162dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe09837cf531e8b45d5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4050ac1afd388db6cc7e",
+    "url": "/static/js/2.62478659.chunk.js"
+  },
+  {
+    "revision": "fbe09837cf531e8b45d5",
+    "url": "/static/js/main.8e0427fc.chunk.js"
+  },
+  {
+    "revision": "50cb9aa5f15f329e71d6",
+    "url": "/static/js/runtime-main.576dec9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
